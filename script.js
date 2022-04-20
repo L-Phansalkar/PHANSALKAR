45174335ef28bd4759ce5af5cdd9e2214b7c7b5e
LPM <11pm@LPMs-MacBook-Air.local>
2022-04-20 17:54:18 -0400

    fixed click issues

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const aboutMeClick = document.getElementById("aboutmeclick");
 aboutMeClick.addEventListener("click", () => clickToRevealAbout());
 
 function clickToRevealFacts() {
-  var aboutme = document.getElementById("aboutme");
-  aboutme.classList.toggle("hidden");
+  var funfact = document.getElementById("funfac");
+  funfact.classList.toggle("hidden");
 }
 const faqClick = document.getElementById("faq");
 faqClick.addEventListener("click", () => clickToRevealFacts());
